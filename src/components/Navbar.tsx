787f7d08bc34fb7d05242018e6618e82edee07c3
midul <midulislamrasel01@gmail.com>
2024-11-21 17:03:04 +0600

    Side Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+//==========
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import CloseIcon from "@/utils/icons/CloseIcon";
+import NavData from "@/utils/mock/NavData/NavData";
+import LogoIcon from "@/utils/icons/logoIcon";
+import HomeIcon from "@/utils/icons/HomeIcon";
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Function to toggle sidebar visibility
+    const toggleSidebar = () => setIsOpen(!isOpen);
+
+    return (
+        <div className="flex h-screen">
+            {/* Sidebar */}
+            <div
+                className={`fixed top-0 left-0  text-black p-5 transition-all duration-300 h-full shadow-lg ${
+                    isOpen ? "w-64" : "w-24"
+                }`}
+            >
+                {/* Toggle Sidebar Button */}
+                <button
+                    className="  rounded text-3xl text-gray-200 mb-8 focus:outline-none "
+                    onClick={toggleSidebar}
+                >
+                    {isOpen ? (
+                        <div className="flex items-center gap-2">
+                            <span className="text-lg font-bold">
+                                <LogoIcon />
+                            </span>
+                        </div>
+                    ) : (
+                        <LogoIcon />
+                    )}
+                </button>
+
+                {/* ===========Map start======= */}
+                <ul className="space-y-4">
+                    {NavData.map(({ id, text, Icon }) => (
+                        <li key={id}>
+                            <Link href="/">
+                                <span className="block py-2 px-4 hover:bg-blue-600 hover:text-white rounded cursor-pointer transition-colors">
+                                    {isOpen ? (
+                                        <div className="flex items-center gap-3">
+                                            <Icon /> {text}
+                                        </div>
+                                    ) : (
+                                        <Icon />
+                                    )}
+                                </span>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                {/* ===========Map End ======== */}
+            </div>
+        </div>
+    );
+}
---
+++ src/utils/mock/NavData/NavData.tsx
@@ -0,0 +1,45 @@
+import HomeIcon from "@/utils/icons/HomeIcon";
+import OrderIcon from "@/utils/icons/OrderIcon";
+import ProfileIcon from "@/utils/icons/ProfileIcon";
+import TaskIcon from "@/utils/icons/TaskIcon";
+import TaskIcon2 from "@/utils/icons/TaskIcon2";
+
+const NavData = [
+    {
+        id: "nav1",
+        text: "Manage Task",
+        Icon: HomeIcon,
+    },
+    {
+        id: "nav2",
+        text: "Create Task",
+        Icon: OrderIcon,
+    },
+    {
+        id: "nav3",
+        text: "To-do Task",
+        Icon: TaskIcon,
+    },
+    {
+        id: "nav4",
+        text: "OnGoinge Task",
+        Icon: TaskIcon2,
+    },
+    {
+        id: "nav5",
+        text: "Completed Task",
+        Icon: ProfileIcon,
+    },
+    {
+        id: "nav6",
+        text: "My Profile",
+        Icon: TaskIcon,
+    },
+    {
+        id: "nav7",
+        text: "Home",
+        Icon: HomeIcon,
+    },
+];
+
+export default NavData;
